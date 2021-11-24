80323345a2fa784a4d7e537bfb7fce8c3f31ebb5
Bruno Carrard <brunocarrardoliveira@gmail.com>
2021-11-24 19:26:46 -0300

    funcao calcular IMC atualizada para calcular de todos os pacientes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-const paciente = document.querySelector('[data-primeiro-paciente]');
-const tdPeso = paciente.querySelector('[data-peso]');
-const tdAltura = paciente.querySelector('[data-altura]');
-const tdImc = paciente.querySelector('[data-imc]');
+const pacientes = document.querySelectorAll('[data-paciente]');
 
-const peso = tdPeso.textContent;
-const altura = tdAltura.textContent;
+for (i = 0; i < pacientes.length; i++) {
+    const paciente = pacientes[i] 
 
-const imc = peso / (altura * altura);
-
-var alturaValida = true;
-var pesoValido = true;
-
-if (peso < 0 || peso >300) {
-    console.log('peso invalido')
-    pesoValido = false
-}
-
-if (altura <= 0 || altura >= 3) {
-    console.log('altura invalida')
-    alturaValida = false
-}
-
-if (alturaValida == true && pesoValido == true) {
-    tdImc.textContent = imc
-}else{
-    tdImc.textContent = "Altura ou peso inválidos"
+    const tdPeso = paciente.querySelector('[data-peso]');
+    const tdAltura = paciente.querySelector('[data-altura]');
+    const tdImc = paciente.querySelector('[data-imc]');
+    
+    const peso = tdPeso.textContent;
+    const altura = tdAltura.textContent;
+    
+    const imc = peso / (altura * altura);
+    
+    var alturaValida = true;
+    var pesoValido = true;
+    
+    if (peso < 0 || peso >300) {
+        console.log('peso invalido')
+        pesoValido = false
+    }
+    
+    if (altura <= 0 || altura >= 3) {
+        console.log('altura invalida')
+        alturaValida = false
+    }
+    
+    if (alturaValida == true && pesoValido == true) {
+        tdImc.textContent = imc.toFixed(2)
+    }else{
+        tdImc.textContent = "Altura ou peso inválidos"
+    }
 }
